3c880aea18cebc76b56a421b8cff5b0d2513db4a
IISResetMe <mathias@mrjessen.com>
2021-12-02 21:45:28 +0100

    AoC 2021: Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use aocio::load_input;
-use std::env;
+use std::{env, str::FromStr, string::ParseError};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,6 +15,7 @@ fn main() {
 
     match day_parse_result.unwrap() {
         1 => day_1(),
+        2 => day_2(),
         _ => show_usage()
     }
 }
@@ -53,4 +54,60 @@ fn day_1() {
     }
 
     println!("Part 2: {}", count);
+}
+
+fn day_2() {
+    struct DiveInstruction {
+        direction: Box<str>,
+        distance: u32,
+    }
+
+    impl FromStr for DiveInstruction {
+        type Err = ParseError;
+        fn from_str(input: &str) -> Result<Self, Self::Err> {
+            let mut chunks = input.split(" ");
+            let direction = chunks.next().unwrap().into();
+            let distance = chunks.next().unwrap().parse::<u32>().unwrap();
+
+            return Ok(DiveInstruction { direction: direction, distance: distance });
+        }
+    }
+
+    // io and prep
+    let filepath = env::current_dir().unwrap().as_path().join("data\\day02.txt");
+    let lines = load_input(filepath.into_os_string().to_str().unwrap());
+
+    let instructions = lines.iter().map(|l| DiveInstruction::from_str(&l).unwrap());
+
+    // part 1
+    let mut x = 0;
+    let mut y = 0;
+
+    for instr in instructions {
+        match (*instr.direction).as_ref() {
+            "forward" => x = x + instr.distance,
+            "down" => y = y + instr.distance,
+            "up" => y = y - instr.distance,
+            _ => ()
+        }
+    }
+
+    println!("Part 1: {}", x * y);
+
+    // part 2
+    let mut aim =0;
+    x = 0;
+    y = 0;
+
+    let instructions = lines.iter().map(|l| DiveInstruction::from_str(&l).unwrap());
+    for instr in instructions {
+        match (*instr.direction).as_ref() {
+            "forward" => {x = x + instr.distance; y = y + aim * instr.distance},
+            "down" => aim = aim + instr.distance,
+            "up" => aim = aim - instr.distance,
+            _ => ()
+        }
+    }
+
+    println!("Part 2: {}", x * y);
 }
